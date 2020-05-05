cd3ff77726bfdce4c69f9bffd88f5da9a8c50623
Maicol Andrés Garzón Fonseca <55245364+MikeGarzon@users.noreply.github.com>
2020-05-05 13:31:57 -0500

    Add files via upload

---
+++ Cronometro.py
@@ -0,0 +1,8 @@
+import time
+
+for h in range(0, 24):
+    for m in range(0, 60):
+        for s in range(0, 60):
+            for ds in range(0, 10):
+                print(h, "S", m, "M:", s, "S:", ds, "ds")
+                time.sleep(0.10)  #éste metedo salta cada segundo, por eso 0.10
